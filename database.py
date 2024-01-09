2522c7511a6ee75dea2a4e61849863a890212c4f
Giuseppe Pio Petillo <giuseppe99petillo@gmail.com>
2024-01-09 22:37:21 +0100

    update
    update

--- database.py
+++ database.py
@@ -7,6 +7,27 @@ from streamlit_image_select import image_select
 #Questo 
 
 st.set_page_config(page_icon="logo.png")
+
+
+background_image = """
+<style>
+[data-testid="stAppViewContainer"] > .main {
+    background-image: url("https://unsplash.com/it/foto/cielo-blu-con-sole-giallo-UXUYv_yp9QU");
+    background-size: 100vw 100vh;  
+    background-position: center;  
+    background-repeat: no-repeat;
+}
+</style>
+"""
+
+
+st.markdown(background_image, unsafe_allow_html=True)
+
+#Viola scuro
+#7a5ea8
+
+
+
 logo = "logo.png"
 # Controllo IF
 is_aut = False
@@ -32,11 +53,11 @@ if st.sidebar.button("Login"):
     # Controllo di autenticazione
     if authenticate(email, password):
         st.sidebar.success("Accesso riuscito!")
+        st.success(f"benvenut*{email}")
         is_aut = True
         # Qui puoi aggiungere ulteriori sezioni del tuo codice per il contenuto principale
     else:
         st.sidebar.error("Accesso fallito. Verifica email e password.")
-
 
 
 
@@ -47,72 +68,71 @@ existing_data = conn.read(worksheet="Foglio1", usecols=list(range(4)), ttl=5)
 existing_data = existing_data.dropna(how="all")
 
 
-
-
-
 giocatori = [
-             {"nome": "Annalisa Cantatore", "crediti": 7},
-    {"nome": "Anton Luca Leggiero", "crediti": 9},
-    {"nome": "Camilla Marcone", "crediti": 3},
-    {"nome": "Castrese Izzo", "crediti": 5},
-    {"nome": "Chiara Iannicelli", "crediti": 6},
-    {"nome": "Clarissa Fionda", "crediti": 4},
-    {"nome": "Daniele Fotso", "crediti": 5},
-    {"nome": "Edoardo Cesaroni", "crediti": 6},
-    {"nome": "Elena Kiseleva", "crediti": 7},
-    {"nome": "Eleonora Carta", "crediti": 3},
-    {"nome": "Emanuele Dias Fernandes", "crediti": 5},
-    {"nome": "Eugenio Francesco Pensa", "crediti": 4},
-    {"nome": "Fabio Ciccarelli", "crediti": 3},
-    {"nome": "Fabio Di Corpo", "crediti": 7},
-    {"nome": "Federica Lavini", "crediti": 5},
-    {"nome": "Flavia Sarcinelli", "crediti": 11},
-    {"nome": "Francesco Antonelli", "crediti": 9},
-    {"nome": "Francesco Arpino", "crediti": 9},
-    {"nome": "Francesco Cartisano", "crediti": 5},
-    {"nome": "Francesco Tarantino", "crediti": 9},
-    {"nome": "Francesco Tonnarini", "crediti": 8},
-    {"nome": "Gabriele La Motta", "crediti": 8},
-    {"nome": "Giacomo Bacchetta", "crediti": 5},
-    {"nome": "Giada De Cupis", "crediti": 9},
-    {"nome": "Gianpio Tomasso", "crediti": 1},
-    {"nome": "Giorgia Scaglione", "crediti": 1},
-    {"nome": "Giovanni Caldarini", "crediti": 1},
-    {"nome": "Giovanni Perugini", "crediti": 11},
-    {"nome": "Giulia Arcifa", "crediti": 2},
-    {"nome": "Giulia Di Giuseppantonio", "crediti": 7},
-    {"nome": "Giulia Milone", "crediti": 7},
-    {"nome": "Giulia Ragazzo", "crediti": 8},
-    {"nome": "Giuseppe Della Greca", "crediti": 9},
-    {"nome": "Giuseppe Maiese", "crediti": 9},
-    {"nome": "Giuseppe Pio Petillo", "crediti": 1},
-    {"nome": "Graziana Antonacci", "crediti": 9},
-    {"nome": "Linda Lezzi", "crediti": 4},
-    {"nome": "Lorenzo Brecevich", "crediti": 5},
-    {"nome": "Lorenzo Saccucci", "crediti": 6},
-    {"nome": "Ludovico Pomanti", "crediti": 8},
-    {"nome": "Marco Bellomo", "crediti": 7},
-    {"nome": "Marco Ponzuoli", "crediti": 8},
-    {"nome": "Matteo Cargini", "crediti": 1},
-    {"nome": "Michela Casale", "crediti": 6},
-    {"nome": "Michele Vitulli", "crediti": 6},
-    {"nome": "Mihai Rosu", "crediti": 7},
-    {"nome": "Pietro Colaguori", "crediti": 5},
-    {"nome": "Rebecca Villani", "crediti": 1},
-    {"nome": "Riccardo Cegna", "crediti": 4},
-    {"nome": "Roberta Ioffredo", "crediti": 11},
-    {"nome": "Sara Valletta", "crediti": 11},
-    {"nome": "Silvia Ricciarello", "crediti": 4},
-    {"nome": "Stefano Candela", "crediti": 8},
-    {"nome": "Valentina Alfano", "crediti": 7},
-    {"nome": "Valentina Giuliani", "crediti": 9},
-    {"nome": "Valentina Nerone", "crediti": 10},
-    {"nome": "Valerio Fiorentino", "crediti": 7},
-    {"nome": "Valerio Scansalegna", "crediti": 8},
-    {"nome": "Victoria Martellotta", "crediti": 4},
-        ]
+    
+    {"nome": "Annalisa Cantatore (7 crediti)", "crediti": 7},
+    {"nome": "Anton Luca Leggiero (9 crediti)", "crediti": 9},
+    {"nome": "Camilla Marcone (3 crediti)", "crediti": 3},
+    {"nome": "Castrese Izzo (5 crediti)", "crediti": 5},
+    {"nome": "Chiara Iannicelli (6 crediti)", "crediti": 6},
+    {"nome": "Clarissa Fionda (4 crediti)", "crediti": 4},
+    {"nome": "Daniele Fotso (5 crediti)", "crediti": 5},
+    {"nome": "Edoardo Cesaroni (6 crediti)", "crediti": 6},
+    {"nome": "Elena Kiseleva (7 crediti)", "crediti": 7},
+    {"nome": "Eleonora Carta (3 crediti)", "crediti": 3},
+    {"nome": "Emanuele Dias Fernandes (5 crediti)", "crediti": 5},
+    {"nome": "Eugenio Francesco Pensa (4 crediti)", "crediti": 4},
+    {"nome": "Fabio Ciccarelli (3 crediti)", "crediti": 3},
+    {"nome": "Fabio Di Corpo (7 crediti)", "crediti": 7},
+    {"nome": "Federica Lavini (5 crediti)", "crediti": 5},
+    {"nome": "Flavia Sarcinelli (11 crediti)", "crediti": 11},
+    {"nome": "Francesco Antonelli (9 crediti)", "crediti": 9},
+    {"nome": "Francesco Arpino (9 crediti)", "crediti": 9},
+    {"nome": "Francesco Cartisano (5 crediti)", "crediti": 5},
+    {"nome": "Francesco Tarantino (9 crediti)", "crediti": 9},
+    {"nome": "Francesco Tonnarini (8 crediti)", "crediti": 8},
+    {"nome": "Gabriele La Motta (8 crediti)", "crediti": 8},
+    {"nome": "Giacomo Bacchetta (5 crediti)", "crediti": 5},
+    {"nome": "Giada De Cupis (9 crediti)", "crediti": 9},
+    {"nome": "Gianpio Tomasso (1 credito)", "crediti": 1},
+    {"nome": "Giorgia Scaglione (1 credito)", "crediti": 1},
+    {"nome": "Giovanni Caldarini (1 credito)", "crediti": 1},
+    {"nome": "Giovanni Perugini (11 crediti)", "crediti": 11},
+    {"nome": "Giulia Arcifa (2 crediti)", "crediti": 2},
+    {"nome": "Giulia Di Giuseppantonio (7 crediti)", "crediti": 7},
+    {"nome": "Giulia Milone (7 crediti)", "crediti": 7},
+    {"nome": "Giulia Ragazzo (8 crediti)", "crediti": 8},
+    {"nome": "Giuseppe Della Greca (9 crediti)", "crediti": 9},
+    {"nome": "Giuseppe Maiese (9 crediti)", "crediti": 9},
+    {"nome": "Giuseppe Pio Petillo (1 credito)", "crediti": 1},
+    {"nome": "Graziana Antonacci (9 crediti)", "crediti": 9},
+    {"nome": "Linda Lezzi (4 crediti)", "crediti": 4},
+    {"nome": "Lorenzo Brecevich (5 crediti)", "crediti": 5},
+    {"nome": "Lorenzo Saccucci (6 crediti)", "crediti": 6},
+    {"nome": "Ludovico Pomanti (8 crediti)", "crediti": 8},
+    {"nome": "Marco Bellomo (7 crediti)", "crediti": 7},
+    {"nome": "Marco Ponzuoli (8 crediti)", "crediti": 8},
+    {"nome": "Matteo Cargini (1 credito)", "crediti": 1},
+    {"nome": "Michela Casale (6 crediti)", "crediti": 6},
+    {"nome": "Michele Vitulli (6 crediti)", "crediti": 6},
+    {"nome": "Mihai Rosu (7 crediti)", "crediti": 7},
+    {"nome": "Pietro Colaguori (5 crediti)", "crediti": 5},
+    {"nome": "Rebecca Villani (1 credito)", "crediti": 1},
+    {"nome": "Riccardo Cegna (4 crediti)", "crediti": 4},
+    {"nome": "Roberta Ioffredo (11 crediti)", "crediti": 11},
+    {"nome": "Sara Valletta (11 crediti)", "crediti": 11},
+    {"nome": "Silvia Ricciarello (4 crediti)", "crediti": 4},
+    {"nome": "Stefano Candela (8 crediti)", "crediti": 8},
+    {"nome": "Valentina Alfano (7 crediti)", "crediti": 7},
+    {"nome": "Valentina Giuliani (9 crediti)", "crediti": 9},
+    {"nome": "Valentina Nerone (10 crediti)", "crediti": 10},
+    {"nome": "Valerio Fiorentino (7 crediti)", "crediti": 7},
+    {"nome": "Valerio Scansalegna (8 crediti)", "crediti": 8},
+    {"nome": "Victoria Martellotta (4 crediti)", "crediti": 4},
+]
 
 
+#agg nome su 
 
 def seleziona_giocatori():
 
@@ -137,7 +157,7 @@ def seleziona_giocatori():
         st.write(f"Hai ancora {20-crediti_totali} Crediti ")
 
         if crediti_totali <= 20:
-            continua_selezione = st.checkbox("Vuoi selezionare un altro giocatore?", key=crediti_totali + 1)
+            continua_selezione = st.checkbox("Vuoi selezionare un altro giocatore?", key=crediti_totali +1 )
             if continua_selezione:
                 numeri += 1
             if not continua_selezione:
@@ -145,7 +165,7 @@ def seleziona_giocatori():
 
     st.sidebar.write("Il tuo Team:")
     for giocatore in giocatori_selezionati:
-        st.sidebar.write(f"- {giocatore['nome']} ({giocatore['crediti']} crediti)")
+        st.sidebar.write(f"- {giocatore['nome']} ")
 
     submit_button = st.button("Aggiorna il team")
 
@@ -170,35 +190,12 @@ def seleziona_giocatori():
 
 
 def main():
-    st.title("FantaJesapper")
-    
+    st.title("FantaJesaper")
+
     seleziona_giocatori()
     
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
